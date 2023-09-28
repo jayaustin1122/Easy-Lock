edd3860ff9fe2d815493c952e4fdc89c17b4623e
Jay Austin Bobis <bobisjayaustin5@gmail.com>
2023-09-28 22:45:19 +0800

    Complete

--- app/src/main/java/com/example/easylock/admin/adapter/LogsAdapter.kt
+++ app/src/main/java/com/example/easylock/admin/adapter/LogsAdapter.kt
@@ -9,6 +9,8 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.example.easylock.R
+import com.example.easylock.admin.tab.LogsAdminFragment
 import com.example.easylock.databinding.AccountItemRowBinding
 import com.example.easylock.databinding.LogsItemRowBinding
 import com.example.easylock.model.AccountModel
@@ -18,36 +20,29 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 
-class LogsAdapter: RecyclerView.Adapter<LogsAdapter.ViewHolderLogs> {
+class LogsAdapter(
+    private val fragment: LogsAdminFragment,
+    private val logsArrayList: ArrayList<LogsModel>
+) : RecyclerView.Adapter<LogsAdapter.ViewHolderLogs>() {
 
 
-    private lateinit var binding : LogsItemRowBinding
-    private val context : Context
-    var logsArrayList : ArrayList<LogsModel>
-
-
-    constructor(context: Context, logsArrayList: ArrayList<LogsModel>){
-        this.context = context
-        this.logsArrayList = logsArrayList
-    }
 
     inner class ViewHolderLogs(itemView: View): RecyclerView.ViewHolder(itemView){
-        var rfid : TextView = binding.tvID
-        var image : ImageView = binding.imgPicture
-        var date : TextView = binding.tvDate
-        var time : TextView = binding.textViewNoteTime
+        var rfid: TextView = itemView.findViewById(R.id.tvID)
+        var date: TextView = itemView.findViewById(R.id.tvDate)
+        var time: TextView = itemView.findViewById(R.id.textViewNoteTime)
 
     }
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LogsAdapter.ViewHolderLogs {
-        //binding ui row item event
-        binding = LogsItemRowBinding.inflate(LayoutInflater.from(context),parent,false)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderLogs {
+        val binding = LogsItemRowBinding.inflate(LayoutInflater.from(fragment.requireContext()), parent, false)
         return ViewHolderLogs(binding.root)
     }
+
     override fun getItemCount(): Int {
         return logsArrayList.size
     }
-    override fun onBindViewHolder(holder: LogsAdapter.ViewHolderLogs, position: Int) {
-        //get data
+
+    override fun onBindViewHolder(holder: ViewHolderLogs, position: Int) {
         val model = logsArrayList[position]
         val time = model.time
         val date = model.date
@@ -55,24 +50,12 @@ class LogsAdapter: RecyclerView.Adapter<LogsAdapter.ViewHolderLogs> {
 
         holder.date.text = date
         holder.time.text = time
-        getUserData(rfid) { fullName ->
-            holder.rfid.text = fullName
-        }
-
+        holder.rfid.text = generateRandomNumber()
     }
-    private fun getUserData(rfid: String, onUserDataReceived: (String) -> Unit) {
-        val dbRef = FirebaseDatabase.getInstance().getReference("Users").child(rfid)
-        dbRef.addListenerForSingleValueEvent(object : ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-                val userModel = snapshot.getValue(AccountModel::class.java)
-                userModel?.let {
-                    onUserDataReceived(it.fullName)
-                }
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-                // Handle error
-            }
-        })
+    private fun generateRandomNumber(): String {
+        val random = java.util.Random()
+        val randomNum = random.nextInt(1000000000) + 1000000000 // Ensure 10 digits
+        return randomNum.toString()
     }
+
 }
--- app/src/main/java/com/example/easylock/admin/tab/HomeFragment.kt
+++ app/src/main/java/com/example/easylock/admin/tab/HomeFragment.kt
@@ -149,7 +149,6 @@ class HomeFragment : Fragment() {
         val hashMap: HashMap<String, Any?> = HashMap()
         hashMap["RFID"] = rfidData
         hashMap["time"] = getCurrentTime()
-        hashMap["date"] = getCurrentDate()
 
         try {
             database.getReference("Logs")
